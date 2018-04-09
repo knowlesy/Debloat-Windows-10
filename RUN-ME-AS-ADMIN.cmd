6baa3da7b48e76a232e179e793f52fc8fd919ba5
knowlesy <rockpeteuk@gmail.com>
2018-04-09 19:48:06 +0100

    Update RUN-ME-AS-ADMIN.cmd

--- RUN-ME-AS-ADMIN.cmd
+++ RUN-ME-AS-ADMIN.cmd
@@ -1,10 +1,19 @@
 powershell -command "& {Set-ExecutionPolicy Unrestricted}"
-powershell -file "C:\Temp\scripts\block-telemetry.ps1"
-powershell -file "C:\Temp\scripts\disable-scheduled-tasks.ps1"
-powershell -file "C:\Temp\scripts\disable-services.ps1"
-powershell -file "C:\Temp\scripts\fix-privacy-settings.ps1"
-powershell -file "C:\Temp\scripts\optimize-user-interface.ps1"
-powershell -file "C:\Temp\scripts\optimize-windows-update.ps1"
-powershell -file "C:\Temp\scripts\remove-default-apps.ps1"
+
+powershell -file "C:\Temp\scripts\Debloat-Windows-10-master\block-telemetry.ps1"
+
+powershell -file "C:\Temp\scripts\Debloat-Windows-10-master\disable-scheduled-tasks.ps1"
+
+powershell -file "C:\Temp\scripts\Debloat-Windows-10-master\disable-services.ps1"
+
+powershell -file "C:\Temp\scripts\Debloat-Windows-10-master\fix-privacy-settings.ps1"
+
+powershell -file "C:\Temp\scripts\Debloat-Windows-10-master\optimize-user-interface.ps1"
+
+powershell -file "C:\Temp\scripts\Debloat-Windows-10-master\optimize-windows-update.ps1"
+
+powershell -file "C:\Temp\scripts\Debloat-Windows-10-master\remove-default-apps.ps1"
+
 powershell -command "& {Set-ExecutionPolicy RemoteSigned}"
+powershell start-sleep -seconds 10
 Shutdown /f /r /s
